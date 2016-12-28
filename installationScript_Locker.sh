27ebda39cf956d602ce50a22d52a60d8ad8233d7
Naren Surampudi <nsurampu@gmail.com>
2016-12-28 11:40:51 +0530

    Update installationScript_Locker.sh

--- installationScript_Locker.sh
+++ installationScript_Locker.sh
@@ -2,5 +2,6 @@
 # this will install all the packages necessary for running this project
 
 sudo pip install --upgrade pip #install pip
-sudo pip install PyPDF2 #installs pytube
+sudo pip install PyPDF2 #installs PyPDF2
+sudo pip install pycrypto #install pycrypto
 echo "Installation successful. You may now run the project."
