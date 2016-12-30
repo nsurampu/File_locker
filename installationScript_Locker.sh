bb5573c9e753ba50ac45c0b8f5b3b2ab76f09a23
Naren Surampudi <nsurampu@gmail.com>
2016-12-30 11:09:54 +0530

    Update installationScript_Locker.sh

--- installationScript_Locker.sh
+++ installationScript_Locker.sh
@@ -4,4 +4,5 @@
 sudo pip install --upgrade pip #install pip
 sudo pip install PyPDF2 #installs PyPDF2
 sudo pip install pycrypto #install pycrypto
+sudo apt-get install python-tk #installs Tkinter
 echo "Installation successful. You may now run the project."
