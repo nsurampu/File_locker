d6e2133aeef90fbe3c30fda7408c57092456c826
Naren Surampudi <nsurampu@gmail.com>
2017-01-19 15:01:22 +0530

    Update WOC2016.py

--- WOC2016.py
+++ WOC2016.py
@@ -9,35 +9,35 @@ from Crypto.Cipher import AES
 from Crypto import Random
 from Tkinter import *
 from tkMessageBox import *
-from tkFileDialog import *
+ from tkFileDialog import *
 
 BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
-unpad = lambda s: s [:-ord(s[len(s) - 1:])]
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)   #This will ensure that the text in the files are mutiples of 16
+unpad = lambda s: s [:-ord(s[len(s) - 1:])]   #This will remove the 'pads' that were added to the original text while encrypting
 
-def pdfLock(event):
+def pdfLock(event):   #This function takes care of locking the pdf files and setting a password to them
     try:
         filename = fileEntry.get()
         user_pass = passEntry.get()
-        owner_pass = user_pass
+         owner_pass = user_pass
         input_file = filename
-        output_file = "temp"
+        output_file = "temp"   #Creating a temporary pdf file where all the contents of the original file will be copied to. This will be the one that will be locked
 
         output = PyPDF2.PdfFileWriter()
         inputf = open(input_file, "rb")
 
         input_stream = PyPDF2.PdfFileReader(inputf)
 
-        for i in range(0, input_stream.getNumPages()):
+        for i in range(0, input_stream.getNumPages()):  #Reading the pages in the pdf file and adding them one by one to the 'temp' file
             output.addPage(input_stream.getPage(i))
 
             outputStream = open(output_file, "wb")
 
-        output.encrypt(user_pass, owner_pass, use_128bit = True)
+        output.encrypt(user_pass, owner_pass, use_128bit = True)   #This will set the password of the lock using 128 bit encryption
         output.write(outputStream)
         outputStream.close()
         inputf.close()
-        os.remove(input_file)
+        os.remove(input_file)   #Need to remove the file first to rename the 'temp' file, else won't work on Windows
         os.rename(output_file, input_file)
         statusPop()
         disabler()
@@ -45,7 +45,7 @@ def pdfLock(event):
         showerror("Error", e)
         disabler()
 
-def txtEncrypt(event):
+def txtEncrypt(event):   #This takes care of the txt files encryption
     try:
         input_file = fileEntry.get()
         key = keyEntry.get()
@@ -54,19 +54,19 @@ def txtEncrypt(event):
         plain = ifile.read()
         ifile.close()
         ofile = open(input_file, "w+")
-        plain = pad(plain)
-        IV = Random.new().read(AES.block_size)
-        cipher = AES.new(key, AES.MODE_CBC, IV)
-        encrypted = base64.b64encode(IV + cipher.encrypt(plain))
+         plain = pad(plain)
+        IV = Random.new().read(AES.block_size)   #Creating a random initialization vector
+        cipher = AES.new(key, AES.MODE_CBC, IV)   #Creating the cipher for encrypting the text
+        encrypted = base64.b64encode(IV + cipher.encrypt(plain))   #Encrypting the text
         ofile.write(encrypted)
         ofile.close()
         statusPop()
         disabler()
     except Exception as e:
         showerror("Error", e)
-        disabler()
+         disabler()
 
-def txtDecrypt(event):
+def txtDecrypt(event):   #This takes care of the txt files decryption
     try:
         efile = fileEntry.get()
         key = keyEntry.get()
@@ -76,9 +76,9 @@ def txtDecrypt(event):
         ifile.close()
         ofile = open(efile, "w+")
         enc = base64.b64decode(enc)
-        IV = enc[:16]
-        cipher = AES.new(key, AES.MODE_CBC, IV)
-        decrypted = unpad(cipher.decrypt(enc[16:]))
+        IV = enc[:16]   #Extracting the IV from the encrypted text
+        cipher = AES.new(key, AES.MODE_CBC, IV)   #Creating the cipher for decrypting text
+        decrypted = unpad(cipher.decrypt(enc[16:]))   #Decrypting the text
         ofile.write(decrypted)
         ofile.close()
         statusPop()
@@ -87,7 +87,7 @@ def txtDecrypt(event):
         showerror("Error", e)
         disabler()
 
-def enabler(event):
+def enabler(event):   #Enables the various GUI components
     try:
         text = fileEntry.get()
         ext = text.split(".")[1]
@@ -101,11 +101,12 @@ def enabler(event):
             encryptButton.bind("<Button-1>", txtEncrypt)
             decryptButton.bind("<Button-1>", txtDecrypt)
         else:
+
             showwarning("Warning", "The file type is currently not supported.\nOnly pdf and txt files are allowed")
     except Exception as e:
         showerror("Error", e)
 
-def disabler():
+def disabler():   #Disables the various GUI components
     lockButton.config(state = DISABLED)
     encryptButton.config(state = DISABLED)
     decryptButton.config(state = DISABLED)
@@ -125,7 +126,9 @@ def fileTray():
     tray = askopenfilename(parent = root, title = "Choose file")
     fileEntry.delete(0, END)
     fileEntry.insert(0, tray)
-    
+
+     
+#The GUI for the application
 
 root = Tk()
 root.geometry("420x400+0+0")
